26ef8ac0765833222a2e6d98766796753a9512a4
Janis Kirsteins <janis@janiskirsteins.org>
2018-07-10 17:47:43 +0100

    The initial API v1 endpoints (see below)
    - Endpoint /api/v1/country/<code>/currency
    - Endpoint /api/v1/currency/<code>/country

--- app/app.py
+++ app/app.py
@@ -5,9 +5,10 @@ import logging
 
 from flask import Flask
 from flask import request
+from flask import jsonify
 from flask_injector import FlaskInjector
 
-from data_querier import DataQuerier
+from .data_querier import DataQuerier
 
 def create_injector_modules(logger, country_data_object):
     """Generate modules for the 'injector' dependency injection.
@@ -33,20 +34,26 @@ def create_injector_modules(logger, country_data_object):
             scope=request
         )
 
-    return [configure_logger]
+    return [configure_logger, configure_data_querier]
 
 def create_app(additional_modules=None):
     """Initializes the Flask app."""
     app = Flask(__name__)
     app.config.from_object('app.config')
 
-    @app.route('/')
-    def empty_root():
+    @app.route('/api/v1/country/<country_code>/currency')
+    def api_v1_currencies_for_country(country_code, data_querier: DataQuerier):
         #pylint: disable=unused-variable
-        """Empty root page (for submitting requests that Sqreen can intercept)."""
-        return 'Hello World'
+        """API v1 method for returning the 3-char currency codes associated with a given 2 char territory code."""
+        return jsonify(data_querier.get_currencies(country_code))
 
-    injector_modules = create_injector_modules(app.logger)
+    @app.route('/api/v1/currency/<currency_code>/country')
+    def api_v1_countries_for_currency(currency_code, data_querier: DataQuerier):
+        #pylint: disable=unused-variable
+        """API v1 method for returning the 2-char territory codes that use a given currency (identified by a 3 char code)."""
+        return jsonify(data_querier.get_countries(currency_code))
+
+    injector_modules = create_injector_modules(app.logger, app.config.get("COUNTRY_DATA"))
     if isinstance(additional_modules, list):
         injector_modules = injector_modules + additional_modules
 
